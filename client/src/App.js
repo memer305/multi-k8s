2d272928842a71a666e391935ebae7dcf71cd95d
muhammad.omer.aslam <muhammad.omer.aslam@pwc.com>
2022-06-06 19:15:23 +0200

    random change

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn React with me!
           </a>
           <Link className="App-link" to="/">Home</Link>
           <Link className="App-link" to="/otherpage">Other Page</Link>
